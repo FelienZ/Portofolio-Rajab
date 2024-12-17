ca97ae857d87a10cc3e5a0cf88d1b18ce76310bd
FelienZ <rajabfadh@gmail.com>
2024-12-17 14:53:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const checkIn = document.getElementById('check-in');
+    const checkOut = document.getElementById('check-out');
+    const submitButton = document.querySelector('.submit-button');})
